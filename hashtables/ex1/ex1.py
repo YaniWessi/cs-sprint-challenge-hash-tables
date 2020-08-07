8e925a3de3456947ea6e98d6ea92195b6e2d2dca
Kaela M Gallo <kaelamgallo@Kaelas-MBP.home>
2020-08-07 13:52:38 -0400

    This is hard i am finding my way.

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,18 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
+    # package has
+    # weight limit
+    # item weight
+    # write a function that
+    # finds two items
+    # whose sum of weights
+    # equals the (weight limit)
+    # the function will
+    # return a tuple of intergers that has the following form:
+    # (zero, one)
+
+    #
     # Your code here
 
     return None
